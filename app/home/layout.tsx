7edd06659a83074927e8943bb64a49eddec20380
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2023-05-11 16:08:11 +0300

    add layout boilerplate

---
+++ app/home/layout.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/src/components/navbar"
+
+export default function MainLayout({children}: {children: React.ReactNode}) {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        {children}
+      </main>
+    </div>
+  )
+}
