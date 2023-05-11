b08597f89c9136164192a8b3fa637b6a1c4b7d62
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2023-05-11 22:51:36 +0300

    add navbar component to layout

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Navbar from "@/src/components/navbar"
 
 export default function MainLayout({children}: {children: React.ReactNode}) {
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,18 @@
+import { Link, Stack } from '@mui/material'
+import styled from 'styled-components';
+
+const Nav = styled.nav`
+    height: 75px;
+    width: 100%;
+    background-color: gray;
+  `
+
 export default function Navbar() {
   return (
-    <nav>
-    </nav>
+    <Nav>
+      <Stack direction="row" spacing={2}>
+        <Link href='/home'>Home</Link>
+      </Stack>
+    </Nav>
   )
 }
