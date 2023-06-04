e435859f42b839110445c0316e1eeb3f47be3b5c
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2023-06-04 14:57:23 +0300

    add page file for home page

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useState } from "react"
+import { Box, Stack, Card, Tabs, Tab } from "@mui/material"
+import calendar from "@/src/components/calendar"
+
+export default function Home() {
+  return (
+    <main>
+    </main>
+  )
+}
