6356eeec49b6e66580f5c4985a00e122f1d5ae55
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2023-06-05 08:32:48 +0300

    add firebase auth and firestore

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,7 +20,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default { app, analytics}
+export default { app, analytics, auth, db }
