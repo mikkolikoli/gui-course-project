d4854496927e7730e01958f62dd09482ab55ab7c
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2023-06-06 16:46:06 +0300

    redirect user to login screen when not logged in

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../src/firebaseConfig'
+import { redirect } from 'next/navigation'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -13,6 +17,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      redirect('/login')
+    }
+  })
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
