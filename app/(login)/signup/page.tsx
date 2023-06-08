fadd6f6fd1fa21dc55b44e3d799d91a2e3c68fcd
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2023-06-08 08:08:24 +0300

    style sign up page

--- app/(login)/signup/page.tsx
+++ app/(login)/signup/page.tsx
@@ -1,66 +1,30 @@
 "use client"
 import { useState } from "react"
-// import { auth } from "@/src/firebaseConfig"
-// import { createUserWithEmailAndPassword } from "firebase/auth"
-// import { SignInWithGoogleButton } from "@/src/components/auth/authButtons"
-// import ErrorText from "@/src/components/errorText"
+
 import Link from "next/link"
+import { Stack, TextField, Button } from "@mui/material"
 
 export default function Page() {
     const [ email, setEmail ] = useState("")
     const [ password, setPassword ] = useState("")
     const [ repeatPassword, setRepeatPassword ] = useState("")
-    const [ valid, setValid ] = useState(false)
+    const [ error, setError ] = useState(false)
     const [ errortext, setErrortext] = useState("")
-
-    const checkPassword = () => {
-        if (password === repeatPassword) {
-            setValid(true)
-        }
-        else {
-            setValid(false)
-        }
-    }
 
     const handleEmailChange = (e: any) => {
         setEmail(e.target.value)
     }
     const handlePasswordChange = (e: any) => {
         setPassword(e.target.value)
-        checkPassword();
-
-        if (repeatPassword.length > 0 && !valid) {
-            setErrortext("Passwords don't match")
-        }
     }
 
     const handleRepeatPasswordChange = (e: any) => {
         setRepeatPassword(e.target.value)
-        checkPassword()
-
-        if (!valid) {
-            setErrortext("Passwords don't match")
-        }
     }
+
+    const signUpFormSubmitted = (e: any) => {}
     /*
-    const signUp = (e: any) => {
-        e.preventDefault();
-
-        if (!valid) {
-            return false;
-        }
-
-        createUserWithEmailAndPassword(auth, email, password)
-            .then((userCredential) => {
-                const user = userCredential.user
-                // add user to firestore
-            })
-            .catch((error) => {
-                const errorCode = error.code;
-                const errorMessage = error.message;
-                // alert user
-            })
-    }
+    
     
     <form onSubmit={signUp}>
                 <input type="email" name="email" id="email" value={email} />
@@ -72,12 +36,61 @@ export default function Page() {
                 {valid ? <ErrorText text={errortext} /> : null}
                 <input type="submit" value="Sign up" />
             </form>
+
+            <p>Already have an account? <Link href="/login">Login instead</Link></p>
             */
 
     return (
-        <main>
-            
+        <Stack 
+            component="section" 
+            alignItems="center"
+            justifyContent="center"
+            direction="column" 
+            spacing={2}
+        >
+            <h1>Sign up</h1>
+            <Stack 
+                component="form" 
+                direction="column" 
+                alignItems="center"
+                justifyContent="center"
+                spacing={2} 
+                onSubmit={signUpFormSubmitted}
+            >
+                <TextField
+                    required
+                    type="email"
+                    label="E-mail"
+                    value={email}
+                    onChange={handleEmailChange}
+                    autoComplete="username" />
+                <TextField
+                    required
+                    error={error}
+                    helperText={errortext}
+                    type="password"
+                    label="Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    autoComplete="new-password" />
+                <TextField
+                    required
+                    error={error}
+                    helperText={errortext}
+                    type="password"
+                    label="Repeat password"
+                    value={repeatPassword}
+                    onChange={handleRepeatPasswordChange}
+                    autoComplete="new-password" />
+
+                <Button
+                    variant="contained"
+                    type="submit"
+                >
+                    Sign up
+                </Button>
+            </Stack>
             <p>Already have an account? <Link href="/login">Login instead</Link></p>
-        </main>
+        </Stack>
     )
 }
