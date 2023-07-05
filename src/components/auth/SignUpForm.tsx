8c518c9f9d0c89a6486be8cfa7272b4e047f5d06
mikkolikoli <95377077+mikkolikoli@users.noreply.github.com>
2023-07-05 09:43:47 +0000

    prevent signing up and require login

--- src/components/auth/SignUpForm.tsx
+++ src/components/auth/SignUpForm.tsx
@@ -26,6 +26,7 @@ export default function SignUpForm({ onSuccess }: Props) {
     }
 
     if (!uiError) {
+      /*
       const { result, error } = await signUp(email, password)
 
       if (error) {
@@ -36,6 +37,7 @@ export default function SignUpForm({ onSuccess }: Props) {
         setUiError(false)
         onSuccess()
       }
+      */
     }
   }
 
