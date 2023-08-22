d1b6a9dd7dcefc444993722359bdc19ab3b885c3
Mikko Rajakorpi <mikko.rajakorpi@tuni.fi>
2023-08-22 15:29:39 +0300

    set up programme page

---
+++ src/components/programmes/ProgrammeCalendar.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "@mui/material"
+import Workout from "./Workout"
+
+import { useState } from "react"
+import { Programme } from "@/src/firebase/firestore/objects"
+
+interface ProgrammeCalendarProps {
+  programme: Programme
+}
+
+export default function ProgrammeCalendar() {
+  return (
+    <></>
+  )
+}
+
+interface DayProps {
+  day: String
+}
+
+function Day() {
+  return (
+    <></>
+  )
+}
